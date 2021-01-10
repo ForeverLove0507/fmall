99337a842dd1b1bbc2791d0606b51e099a2c0b5c
feipeng <feipeng@ypshengxian.com>
2021-01-10 17:28:30 +0800

    feat:环境搭建

---
+++ src/main/java/com/example/fmall/mbg/mapper/UmsAdminRoleRelationMapper.java
@@ -0,0 +1,34 @@
+package com.example.fmall.mbg.mapper;
+
+import com.example.fmall.mbg.model.UmsAdminRoleRelation;
+import com.example.fmall.mbg.model.UmsAdminRoleRelationExample;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UmsAdminRoleRelationMapper {
+    int countByExample(UmsAdminRoleRelationExample example);
+
+    int deleteByExample(UmsAdminRoleRelationExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UmsAdminRoleRelation record);
+
+    int insertSelective(UmsAdminRoleRelation record);
+
+    List<UmsAdminRoleRelation> selectByExample(UmsAdminRoleRelationExample example);
+
+    UmsAdminRoleRelation selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") UmsAdminRoleRelation record, @Param("example") UmsAdminRoleRelationExample example);
+
+    int updateByExample(@Param("record") UmsAdminRoleRelation record, @Param("example") UmsAdminRoleRelationExample example);
+
+    int updateByPrimaryKeySelective(UmsAdminRoleRelation record);
+
+    int updateByPrimaryKey(UmsAdminRoleRelation record);
+}
