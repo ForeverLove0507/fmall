630e24c1ec39eff84ccfc4a27e30d4c23dfb2b3e
feipeng <feipeng@ypshengxian.com>
2021-01-10 15:38:29 +0800

    feat:环境搭建

--- src/test/java/com/example/fmall/service/RedisServiceTest.java
+++ src/test/java/com/example/fmall/service/RedisServiceTest.java
@@ -1,33 +1,23 @@
 package com.example.fmall.service;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class RedisServiceTest{
 
     @Autowired
     RedisService redisService;
 
     @Test
-    void set() {
+    public void set() {
         redisService.set("123", "111");
         String s = redisService.get("123");
         System.out.println(s);
     }
 
-    @Test
-    void get() {
-    }
-
-    @Test
-    void expire() {
-    }
-
-    @Test
-    void remove() {
-    }
-
-    @Test
-    void increment() {
-    }
 }
